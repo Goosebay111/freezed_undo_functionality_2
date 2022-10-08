882d5099a9b12edd66e0b05f5af945eaaa2df1fc
George Lee <george.p.lee.iii@gmail.com>
2022-10-08 22:45:41 +1000

    save

--- lib/delete.dart
+++ lib/delete.dart
@@ -4,9 +4,9 @@ NestedClass delete({
   required NestedClass object,
   required NestedClass hierarchy,
 }) {
-  // 1. copy the hierarchy.
+  // 1. Conditionally copy the hierarchy.
   NestedClass? copiedHierarchy = hierarchy.children.contains(object) ? hierarchy : null;
-  // 2. modifies.
+  // 2. Conditionally modifies the children of the hierarchy.
   if (copiedHierarchy != null) {
     return hierarchy.copyWith(children: [
       for (var child in hierarchy.children)
@@ -20,21 +20,4 @@ NestedClass delete({
         delete(object: object, hierarchy: child),
     ],
   );
-  // // 1. copy the hierarchy.
-  // final copiedHierarchy = hierarchy;
-  // {
-  //   if (object == copiedHierarchy) {
-  //     return NestedClass.initial();
-  //   }
-  //   // 2. modifies.
-  //   if (copiedHierarchy.children.contains(object)) {
-  //     copiedHierarchy.children.remove(object);
-  //   } else {
-  //     for (NestedClass child in copiedHierarchy.children) {
-  //       delete(object: object, hierarchy: child);
-  //     }
-  //   }
-  //   // 3. returns copied and modified hierarchy
-  //   return copiedHierarchy.copyWith(children: [...copiedHierarchy.children]);
-  // }
 }
